c6c0ec61ee10ab39ff4da7ba728413096a1b851f
Stefanie Gagnon <stefaniekathryn@gmail.com>
2022-06-30 19:05:44 -0400

    for loop, variables and bar chart

--- 14-Interactive-Web-Visualizations/Instructions/StarterCode/static/js/app.js
+++ 14-Interactive-Web-Visualizations/Instructions/StarterCode/static/js/app.js
@@ -1,24 +1,43 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
 d3.json(url).then(function(data) {
-    console.log(data);  
-  });
-
-  // start for changing selection in dropdown menu
-  //d3.selectAll("#selDataset").on("change", updatePlotly);
+    console.log(data);
+    var names = data.names;
+    d3.select("#selDataset")
+    .selectAll('options')
+     	.data(names)
+      .enter()
+    	.append('option')
+      .text(function (d) { return d; }) // text showed in the menu
+      // .attr("value", function (d) { return d; }) // corresponding value returned by the button
+    });
 
 // bar chart, just a basic bar chart set up
-function initBarchart() {
-  var barChartData = [{
-    x: [0, 10, 20, 30, 40, 50],
-    y: [0, 10, 20, 30, 40, 50],
-    type: 'bar',
-    orientation: 'h'}];
-  var layout = {font: {size: 18}};
-  var config = {responsive: true};
-  Plotly.newPlot('bar', barChartData, layout, config);
+function optionChanged(chosen) {
+  d3.json(url).then(function(data) {
+    var allSamples = data.samples;
+    var otuIds = [];
+    var otuLabels = [];
+    var sampleValues = [];
+    allSamples.forEach(function(row) {
+      if (row.id == chosen) {
+        otuIds = row.otu_ids;
+        otuLabels = row.otu_labels;
+        sampleValues = row.sample_values;
+      }
+    });
+
+    var barChartData = [{
+      x: sampleValues,
+      y: otuIds,
+      type: 'bar',
+      orientation: 'h'}];
+    var layout = {font: {size: 18}};
+    var config = {responsive: true};
+    Plotly.newPlot('bar', barChartData, layout, config);
+  });
 }
-initBarchart();
+
 
 // bubble chart, just a basic bubble chart set up
 // var sampleData = [{
