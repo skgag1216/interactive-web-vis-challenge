8a5b7e548176a841fd6a5e4633454ed67018b818
Stefanie Gagnon <stefaniekathryn@gmail.com>
2022-07-03 16:57:12 -0400

    working on metadata demographics sect

--- 14-Interactive-Web-Visualizations/Instructions/StarterCode/static/js/app.js
+++ 14-Interactive-Web-Visualizations/Instructions/StarterCode/static/js/app.js
@@ -1,5 +1,7 @@
+// start by assigning a var to the url and reading in the data
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
+// read in the datat with d3 and .json and create a function to fill in the dropdown menu
 d3.json(url).then(function(data) {
     console.log(data);
     var names = data.names;
@@ -11,21 +13,35 @@ d3.json(url).then(function(data) {
       .text(function (d) { return d; }) // text showed in the menu
     });
 
-// bar chart, just a basic bar chart set up
-function optionChanged(chosen) {
-  d3.json(url).then(function(data) {
-    var allSamples = data.samples
-    var otuIds = [];
-    var otuLabels = [];
-    var sampleValues = [];
-    allSamples.forEach(function(row) {
-      if (row.id == chosen) {
-        otuIds = row.otu_ids;
-        otuLabels = row.otu_labels;
-        sampleValues = row.sample_values;
+// // * denotes what was entered first, // ** is what's added second after function works with both charts
+function optionChanged(userChosen) {  //*
+  d3.json(url).then(function(data) { // *
+    var allSamples = data.samples // * 
+    var otuIds = []; // *
+    var otuLabels = []; // *
+    var sampleValues = []; // *
+    allSamples.forEach(function(row) { // *
+      if (row.id == userChosen) { // *
+        otuIds = row.otu_ids; // *
+        otuLabels = row.otu_labels; // *
+        sampleValues = row.sample_values; // *
       }
     });
+    var allMetadata = data.metadata // **
+    // var age = [];
+    // var bbtype = [];
+    // var ethnicity = [];
+    // var gender = [];
+    // var id = [];
+    // var location = [];
+    // var wfreq = [];
+      allMetadata.forEach(function(row) { // **
+        if (row.id == userChosen)
+          console.log(d3.keys(row),
+          console.log(d3.values(row))) // ** 
+      });// **
 
+// bar chart, just a basic bar chart set up to fill in
     var barChartdata = [{
       x: sampleValues.slice(0,10).reverse(),
       y: otuIds.slice(0,10).map(otuID => `OTU ${otuID}`).reverse(),
@@ -44,8 +60,7 @@ function optionChanged(chosen) {
       x: 0.05
     }};
     Plotly.newPlot('bar', barChartdata, layout);
-
-    // bubble chart, just a basic bubble chart set up
+    // bubble chart, just a basic bubble chart set up to fill in
     var bubbleChartdata = [{
       x: otuIds.map(otuID => `${otuID}`),
       y: sampleValues,
